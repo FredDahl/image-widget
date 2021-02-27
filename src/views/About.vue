6942b97f8df22da1e555881f42eeda165931739c
Fredrik <distortus@gmail.com>
2021-02-27 01:06:09 +0100

    About view changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="about-container">
     <h1>This is an image gallery demo page</h1>
-    <p> Here could product information and similar be displayed in a future version of this site.</p>
-    <p> At the present time so is this a placeholder area.</p>
+    <p>
+      Here could product information and similar be displayed in a future
+      version of this site.
+    </p>
+    <p>At the present time so is this a placeholder area.</p>
+    <p>This project is made public on GitHub:</p>
+    <a href="https://github.com/FredDahl/image-widget" target="_blank"
+      >GitHub repo for Image Widget</a
+    >
     <BackButton class="back-button" route="./" />
   </div>
 </template>
@@ -15,14 +22,25 @@ export default {
 </script>
 
 <style scoped>
+a {
+  padding: 20px;
+}
+a:link {
+  color: blue;
+}
+a:visited {
+  color: blue;
+}
+a:hover {
+  color: #42b983;
+}
 .about-container {
   height: 100vh;
   background-color: #ebebeb;
   display: flex;
   flex-direction: column;
 }
-.back-button{
-padding-top: 50px;
-  
+.back-button {
+  padding-top: 50px;
 }
 </style>
